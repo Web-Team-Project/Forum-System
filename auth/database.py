694a44c6d0019592220ae18cea7ffed329fd40c9
radoooo11 <radomihaylov11@gmail.com>
2024-04-19 11:48:13 +0300

    added scripts for creating and seeding the database(Only Users table), also uploading the databse

--- auth/database.py
+++ auth/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 Base = declarative_base()
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "sqlite:///./forum.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
+
+
