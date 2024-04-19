7e575c2e4f6a8a89c1ab58c0df51f44637891a4a
Rubinart <k.ivanov19981@gmail.com>
2024-04-19 22:38:45 +0300

    Token authentication fixed

--- auth/database.py
+++ auth/database.py
@@ -2,11 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
+DATABASE_URL = "sqlite:///./forum.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-DATABASE_URL = "sqlite:///./forum.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+from auth.models import Users
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-
+Base.metadata.create_all(bind=engine)
--- auth/token.py
+++ auth/token.py
@@ -1,29 +1,33 @@
 from datetime import datetime, timedelta
+from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
-from fastapi import HTTPException, status
+from fastapi import Depends, HTTPException, status
 
 
 SECRET_KEY = "4f1feeca525de4cdb064656007da3edac7895a87ff0ea865693300fb8b6e8f9c"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRATION_MINS = 30
 
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
-def create_access_token(username: str, user_id: int, expires_in: timedelta or None = None): # type: ignore
+
+def create_access_token(username: str, user_id: int, expires_in: timedelta):
     encode = {"sub": username, "id": user_id}
     expiration = datetime.now() + expires_in
     encode.update({"exp": expiration})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-async def get_current_user(token, SECRET_KEY, algorithm=ALGORITHM):
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[algorithm])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
         if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail="Could not validate credentials.")
+                                detail="Could not validate user.")
         return {"username": username, "id": user_id}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Could not validate credentials.")
+                            detail="Could not validate user.")
