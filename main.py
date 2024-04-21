32d97a9d39fbea22fb7ec9c8c025c192fc261442
Rubinart <k.ivanov19981@gmail.com>
2024-04-21 19:48:58 +0300

    Fixed view category and categories

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers.auth import auth_router
 from routers.topics import topics_router
+from routers.categories import category_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(topics_router)
+app.include_router(category_router)
--- routers/categories.py
+++ routers/categories.py
@@ -1,27 +1,28 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
-from auth.models import CreateTopicRequest, Users
-from auth.token import get_current_user
-from services import category_service
+from auth.models import Category, Topics
 from auth.database import get_db
+
 
 category_router = APIRouter(prefix="/categories", tags=["categories"])
 
+
 @category_router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_topic(topic: CreateTopicRequest,
-                        current_user: Users = Depends(get_current_user),
-                        db: Session = Depends(get_db)):
-    return category_service.create_category(db, category, admin)  #???
+async def create_category():
+    pass
 
-@category_router.get("/")
-async def view_categories(db: Session = Depends(get_db)):
-    categories = category_service.get_categories(db)  # Using function from category_service
-    return categories
 
-@category_router.get("/{category_id}")
-async def view_category(category_id: int, db: Session = Depends(get_db)):
-    category = category_service.get_category(db, category_id)  # Using function from category_service
+@category_router.get("/categories/{category_id}")
+def view_category(category_id: int, db: Session = Depends(get_db)):
+    category = db.query(Category).filter(Category.id == category_id).first()
     if category is None:
-        raise HTTPException(status_code=404, detail="Category not found")
-    return {"category": category, "topics": category.topics}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="Category not found.")
+    topics = db.query(Topics).filter(Topics.category_id == category_id).all()
+    return {"category": category.name, "topics": [topic.title for topic in topics]}
+
+
+@category_router.get("/categories")
+def view_categories(db: Session = Depends(get_db)):
+    categories = db.query(Category).all()
+    return {"categories": [category.name for category in categories]}
