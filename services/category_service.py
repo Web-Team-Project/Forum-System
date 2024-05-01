cfe03532a1de0debf737217b5c0e79452a88ce9d
Rubinart <k.ivanov19981@gmail.com>
2024-05-01 16:10:06 +0300

    Added more checks to read and write access

--- services/category_service.py
+++ services/category_service.py
@@ -55,11 +55,19 @@ def change_visibility():
     pass
 
 # Privacy isn't included yet
+# Also refactor into smaller functions eventually
 def read_access(db: Session, category_id: int, user_id: int, 
                 current_user: User = Depends(get_current_user)):
     if current_user.role != Roles.admin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail="The user is not authorized to give read access.")
+    category = db.query(Category).filter_by(id=category_id).first()
+    if category is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="Category not found.")
+    if not category.is_private:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail="The category is public.")
     access_record = db.query(CategoryAccess).filter_by(category_id=category_id, user_id=user_id).first()
     if access_record is None:
         access_record = CategoryAccess(category_id=category_id, user_id=user_id, read_access=True)
@@ -71,11 +79,19 @@ def read_access(db: Session, category_id: int, user_id: int,
 
 
 # Privacy isn't included yet and user has to have read access to get write access
+# Also refactor into smaller functions eventually
 def write_access(db: Session, category_id: int, user_id: int,
                  current_user: User = Depends(get_current_user)):
     if current_user.role != Roles.admin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail="The user is not authorized to give write access.")
+    category = db.query(Category).filter_by(id=category_id).first()
+    if category is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="Category not found.")
+    if not category.is_private:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail="The category is public.")
     access_record = db.query(CategoryAccess).filter_by(category_id=category_id, user_id=user_id).first()
     if access_record is None:
         access_record = CategoryAccess(category_id=category_id, user_id=user_id, write_access=True)
