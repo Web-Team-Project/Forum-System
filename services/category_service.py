92d73768341bed01d5f729a3efd7ea94380242b4
Rubinart <k.ivanov19981@gmail.com>
2024-05-01 16:22:10 +0300

    Write access also gives read access and refactored

--- services/category_service.py
+++ services/category_service.py
@@ -2,15 +2,13 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy import asc, desc
 from sqlalchemy.orm import Session
 from auth.models import CategoryAccess, CreateCategoryRequest, Category, Topic, User
-from auth.roles import Roles
 from auth.token import get_current_user
+from services.user_service import check_admin_role
 
 
 def create_category(db: Session, category: CreateCategoryRequest, 
                     current_user: User = Depends(get_current_user)):
-    if current_user.role != Roles.admin:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
-                            detail="The user is not authorized to create a category.")
+    check_admin_role(current_user)
     db_category = Category(name=category.name)
     db.add(db_category)
     db.commit()
@@ -51,23 +49,21 @@ def get_topics_in_category(db: Session, category_id: int, skip: int = 0, limit: 
     return topics
 
 
-def change_visibility():
-    pass
-
-# Privacy isn't included yet
-# Also refactor into smaller functions eventually
-def read_access(db: Session, category_id: int, user_id: int, 
-                current_user: User = Depends(get_current_user)):
-    if current_user.role != Roles.admin:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
-                            detail="The user is not authorized to give read access.")
-    category = db.query(Category).filter_by(id=category_id).first()
-    if category is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail="Category not found.")
+def check_if_private(category: Category):
     if not category.is_private:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
                             detail="The category is public.")
+
+
+def change_visibility():
+    pass
+
+# Privacy isn't included yet, cannot be fully tested until then
+def read_access(db: Session, category_id: int, user_id: int, 
+                current_user: User = Depends(get_current_user)):
+    check_admin_role(current_user)
+    category = get_category(db, category_id)
+    check_if_private(category)
     access_record = db.query(CategoryAccess).filter_by(category_id=category_id, user_id=user_id).first()
     if access_record is None:
         access_record = CategoryAccess(category_id=category_id, user_id=user_id, read_access=True)
@@ -79,24 +75,18 @@ def read_access(db: Session, category_id: int, user_id: int,
 
 
 # Privacy isn't included yet and user has to have read access to get write access
-# Also refactor into smaller functions eventually
+# Cannot be fully tested until then
 def write_access(db: Session, category_id: int, user_id: int,
                  current_user: User = Depends(get_current_user)):
-    if current_user.role != Roles.admin:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
-                            detail="The user is not authorized to give write access.")
-    category = db.query(Category).filter_by(id=category_id).first()
-    if category is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail="Category not found.")
-    if not category.is_private:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
-                            detail="The category is public.")
+    check_admin_role(current_user)
+    category = get_category(db, category_id)
+    check_if_private(category)
     access_record = db.query(CategoryAccess).filter_by(category_id=category_id, user_id=user_id).first()
     if access_record is None:
-        access_record = CategoryAccess(category_id=category_id, user_id=user_id, write_access=True)
+        access_record = CategoryAccess(category_id=category_id, user_id=user_id, read_access=True, write_access=True)
         db.add(access_record)
     else:
+        access_record.read_access = True
         access_record.write_access = True
     db.commit()
     return {"message": "Write permission has been granted."}
--- services/user_service.py
+++ services/user_service.py
@@ -1,3 +1,8 @@
+from fastapi import HTTPException, status
+from auth.models import User
+from auth.roles import Roles
+
+
 def create_token(user_data):
     pass
 
@@ -8,3 +13,9 @@ def register_user(user_data):
 
 def view_privilaged_users():
     pass
+
+
+def check_admin_role(current_user: User):
+    if current_user.role != Roles.admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+                            detail="The user is not authorized to perform this action.")
