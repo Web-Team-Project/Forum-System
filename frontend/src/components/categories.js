0cafe7091942d72acd0714f48f8d296e21489de1
Rubinart <k.ivanov19981@gmail.com>
2024-05-06 16:26:55 +0300

    Added lock category button

--- frontend/src/components/categories.js
+++ frontend/src/components/categories.js
@@ -68,6 +68,19 @@ const Categories = () => {
     }
   };
 
+  const lockCategory = async (categoryId) => {
+    try {
+      const response = await api.put(`/categories/lock/${categoryId}`, {}, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div style={{ border: "5px solid #4CAF50", padding: "20px", margin: "10px", fontFamily: "Arial, sans-serif" }}>
       <h1 style={{ borderBottom: "2px solid #4CAF50", color: "#333", marginBottom: "20px" }}>Categories</h1>
@@ -75,7 +88,8 @@ const Categories = () => {
         <div key={category.id} style={{ margin: "20px 0", padding: "10px", border: "1px solid #4CAF50", borderRadius: "5px", boxShadow: "0 2px 5px rgba(0,0,0,0.15)" }}>
           <h2 style={{ color: "#333", marginBottom: "10px" }}>{category.name}</h2>
           <button style={{ backgroundColor: "#ddd", color: "#333", marginRight: "10px", cursor: "pointer", padding: "5px 10px", border: "none", borderRadius: "5px", transition: "background-color 0.3s ease" }} onClick={() => viewCategory(category.id)} onMouseOver={(e) => e.target.style.backgroundColor = "#bbb"} onMouseOut={(e) => e.target.style.backgroundColor = "#ddd"}>View Category</button>
-          <button style={{ backgroundColor: "#ddd", color: "#333", cursor: "pointer", padding: "5px 10px", border: "none", borderRadius: "5px", transition: "background-color 0.3s ease" }} onClick={() => changeVisibility(category.id)} onMouseOver={(e) => e.target.style.backgroundColor = "#bbb"} onMouseOut={(e) => e.target.style.backgroundColor = "#ddd"}>Change Visibility</button>
+          <button style={{ backgroundColor: "#ddd", color: "#333", marginRight: "10px", cursor: "pointer", padding: "5px 10px", border: "none", borderRadius: "5px", transition: "background-color 0.3s ease" }} onClick={() => changeVisibility(category.id)} onMouseOver={(e) => e.target.style.backgroundColor = "#bbb"} onMouseOut={(e) => e.target.style.backgroundColor = "#ddd"}>Change Visibility</button>
+          <button style={{ backgroundColor: "#ddd", color: "#333", marginRight: "10px", cursor: "pointer", padding: "5px 10px", border: "none", borderRadius: "5px", transition: "background-color 0.3s ease" }} onClick={() => lockCategory(category.id)} onMouseOver={(e) => e.target.style.backgroundColor = "#bbb"} onMouseOut={(e) => e.target.style.backgroundColor = "#ddd"}>Lock Category</button>
         </div>
       ))}
       <input type="text" value={newCategoryName} onChange={e => setNewCategoryName(e.target.value)} placeholder="" style={{ margin: "20px 0", padding: "5px", width: "100%", border: "1px solid #ddd", borderRadius: "5px" }} />
